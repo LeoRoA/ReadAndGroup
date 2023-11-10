1df3e8031bec13f1074355f251db48f5ebadbcf0
LeoRoA <113158247+LeoRoA@users.noreply.github.com>
2023-11-10 10:52:35 +0300

    unworked

--- src/main/java/org/testcase/Main.java
+++ src/main/java/org/testcase/Main.java
@@ -4,32 +4,28 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 
 public class Main {
-    public static void main(String[] args) {
-//        if (args.length != 1) {
-//            System.out.println("Usage: java -jar <название проекта>.jar <тестовый-файл.txt>");
-//            System.exit(1);
-//        }
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Runtime.getRuntime().availableProcessors());
         long time = System.currentTimeMillis();
         String inputFilePath = "src/main/resources/lng-4.txt.gz";
-//        Map<Integer, Set<List<String>>> groups = new HashMap<>();
-        HashSet<String> groups = new HashSet<>();
+        List<String> groups = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(
                         new GZIPInputStream(Files.newInputStream(new File(inputFilePath).toPath()))))) {
             String line;
             Pattern pattern = Pattern.compile("^(\"?\\w*\"?;)*(\"?\\w*\"?)+$");
-            int i = 0;
             while ((line = br.readLine()) != null && !line.isEmpty()) {
                 Matcher matcher = pattern.matcher(line);
                 if (matcher.matches()) {
-//                    List<String> elements = Arrays.asList(line.split(";"));
                     groups.add(line);
                 }
             }
@@ -39,103 +35,98 @@ public class Main {
         }
         System.out.println(groups.size());
 
-//        // Sort the groups by size in descending order
         List<List<String>> sortedGroups = new ArrayList<>();
         for (String string : groups) {
             List<String> line = Arrays.asList(string.split(";"));
             sortedGroups.add(line);
         }
         sortedGroups.sort(Comparator.comparingInt(List::size));
-////        List<Map.Entry<Integer, Set<List<String>>>> sortedGroups = new ArrayList<>(groups.entrySet());
-////        sortedGroups.sort((e1, e2) -> e2.getKey().compareTo(e1.getKey()));
-////        System.out.println("sg: " + sortedGroups.size());
-//
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-//            int groupCount = 0;
-////            int sizeCount = sortedGroups.get(0).getKey() + 1;
-//            List<List<String>> addedGroup = new ArrayList<>();
-//
-//            // исправить на итератор!!!!!!!!!!
-//            Iterator<List<String>> iterator = sortedGroups.iterator();
-//            int fixSize = sortedGroups.size();
-//            for (int i=fixSize-1; i>0; i--){
-//                groupCount++;
-//                List<String> line = sortedGroups.get(i);
-//                addedGroup.add(line);
-//                writer.write("Группа " + groupCount + "\n");
-//                writer.write(line + "\n");
-//
-//                for (int j = 0; j < line.size() - 1; j++) {
-//                    final int position = j;
-//                    addedGroup.addAll(sortedGroups.stream()
-//                                    .
-//                            .filter(e -> (e.size() - 1 <= position && e.get(position).equals(line.get(position))))
-//                            .collect(Collectors.toList()));
-//
-//                }
-//                writer.write("Группа " + groupCount + "\n");
-//                    for (List<String> addedLine : addedGroup) {
-//                        writer.write(addedLine.toString() + "\n");
-//                    }
-////                writer.write(group + "\n");
-//
-//
-//            }
-//            System.out.println("Количество групп с более чем одним элементом: " + groupCount);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
 
-
-//        for (int i = 0; i < groups.size(); i++) {
-//            Set<List<String>> groupA = groups.get(i);
-//
-//            for (int j = i + 1; j < groups.size(); j++) {
-//                Set<List<String>> groupB = groups.get(j);
-//
-//                if (haveCommonValuesAtSamePositions(groupA, groupB)) {
-//                    groupA.addAll(groupB);
-//                    groups.remove(j);
-//                    j--;
-//                }
-//            }
-//        }
-        List<List<List<String>>> result = groupStrings(data, 1);
-
-
-
-        time = System.currentTimeMillis() - time;
-        System.out.printf("Elapsed %,9.3f ms\n", time / 1_000.0);
-    }
-    private static List<List<String>> groupStrings(List<String> addedLine, List<List<String>>checkedList){
-
+        int numThreads = 4; // Количество потоков для параллельной обработки
+        List<List<List<String>>> groupedLists = groupStrings(sortedGroups, numThreads);
+//        System.out.println(groupedLists.size());
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-            List<List<String>> addedGroup = new ArrayList<>();
-
-            while (checkedList.size()!=0){
-                List<String> addedLine = checkedList.get(0);
-                checkedList.remove(0);
-                addedGroup.add(addedLine);
-//                int lineSize = addedGroup.get(i).size();
-                for (int i = 0; i<addedLine.size()-1;i++) {
-                    String exitingString = addedLine.get(i);
-                    for (List<String> checkedLine: checkedList){
-                        if (checkedLine.size()>=i && checkedLine.get(0).equals(exitingString){
-                            addedGroup.add(checkedLine);
-                        }
-                    }
+            int i = 1;
+            for (List<List<String>> group : groupedLists) {
+                writer.write("Группа " + i++ + "\n");
+                for (List<String> line : group) {
+                    writer.write(String.join(";", line) + "\n");
                 }
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
-
-
-
-
+        System.out.println(groupedLists.size());
+        time = System.currentTimeMillis() - time;
+        System.out.printf("Elapsed %,9.3f ms\n", time / 1_000.0);
     }
+
+    private static List<List<List<String>>> groupStrings(List<List<String>> checkedList, int numThreads) throws InterruptedException {
+        List<List<List<String>>> groupedLists = new ArrayList<>();
+        boolean[] checked = new boolean[checkedList.size()];
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        for (int currentRowNumber = checkedList.size() - 1; currentRowNumber > 0; currentRowNumber--) {
+            List<String> currentRow = checkedList.get(currentRowNumber);
+
+
+            final int rowNumb = currentRowNumber;
+
+            if (!checked[rowNumb]) {
+                List<List<String>> group = new ArrayList<>();
+                group.add(currentRow);
+                checked[rowNumb] = true;
+
+                for (int refRowPos = 0; refRowPos < group.size(); refRowPos++) {
+                    int refElemPos = 0;
+                    String elementToCompare = group.get(refRowPos).get(refElemPos);
+
+                    if (elementToCompare != null && !elementToCompare.equals("\"\"")) {
+                        for (int i = 1; i < numThreads; i++) {
+                            executor.execute(() -> {
+                                try {
+                                    for (int j = 0; j < rowNumb - 1; j++) {
+                                        if (checkedList.get(j).size() > refElemPos && !checked[j] &&
+                                                checkedList.get(j).get(refElemPos).equals(elementToCompare)) {
+                                            group.add(checkedList.get(j));
+                                            checked[j] = true;
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                            });
+                            synchronized (group) {
+                                groupedLists.add(group);
+                            }
+                        }
+
+                    }
+
+
+                }
+//
+                if (currentRowNumber % 1000 == 0) {
+                    System.out.println(currentRowNumber);
+                }
+            }
+        }
+//        System.out.println(groupedLists.size());
+//        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.out.println("Pool did not terminate");
+                }
+////            executor.awaitTermination(30, TimeUnit.SECONDS);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return groupedLists;
+    }
+
 }
